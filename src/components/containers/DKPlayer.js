3c267a52a04edba3f6e4b5856f5a3d446dd4e133
Dario <dkemenovic@gmail.com>
2020-05-25 22:24:23 +0200

    Create component

---
+++ src/components/containers/DKPlayer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Video from "../Video";
+import Playlist from "../containers/Playlist";
+
+const DKPlayer = (props) => {
+  return (
+    <>
+      <Video />
+      <Playlist />
+    </>
+  );
+};
+
+export default DKPlayer;
