3efe860fc28116ccc50a681bd063e752293a513b
Dario <dkemenovic@gmail.com>
2020-05-28 11:50:00 +0200

    Create Playlist component

---
+++ src/components/containers/Playlist.js
@@ -0,0 +1,15 @@
+import React from "react";
+import PlaylistHeader from "../PlaylistHeader";
+import Playlistitems from "../containers/Playlistitems";
+import NightMode from "../Nightmode";
+
+const DKPlayer = (props) => {
+  return (
+    <>
+      <Video />
+      <Playlist />
+    </>
+  );
+};
+
+export default DKPlayer;
