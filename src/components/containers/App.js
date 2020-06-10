0cd9bfe5874a3768862660aa6f590276bea2dece
Dario <dkemenovic@gmail.com>
2020-06-10 14:56:09 +0200

    Import GlobalStyle

--- src/components/containers/App.js
+++ src/components/containers/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import DKPlayer from "./DKPlayer";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+import GlobalStyle from "../styles/GlobalStyle";
 
 const App = () => (
   <BrowserRouter>
@@ -8,6 +9,7 @@ const App = () => (
       <Route exact path="/" component={DKPlayer} />
       <Route exact path="/:activeVideo" component={DKPlayer} />
     </Switch>
+    <GlobalStyle />
   </BrowserRouter>
 );
 
