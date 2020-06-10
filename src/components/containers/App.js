339564588525745fc974af0e3cc91e73b7909e4f
Dario <dkemenovic@gmail.com>
2020-06-10 14:45:18 +0200

    Create Routes

--- src/components/containers/App.js
+++ src/components/containers/App.js
@@ -1,6 +1,14 @@
 import React from "react";
 import DKPlayer from "./DKPlayer";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
-const App = () => <DKPlayer />;
+const App = () => (
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/" component={DKPlayer} />
+      <Route exact path="/:activeVideo" component={DKPlayer} />
+    </Switch>
+  </BrowserRouter>
+);
 
 export default App;
