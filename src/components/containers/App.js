dd7e705d38abe80233f86a8841457d9bfeabc286
Dario <dkemenovic@gmail.com>
2020-06-10 14:58:11 +0200

    Wrap components in fragment

--- src/components/containers/App.js
+++ src/components/containers/App.js
@@ -5,11 +5,13 @@ import GlobalStyle from "../styles/GlobalStyle";
 
 const App = () => (
   <BrowserRouter>
-    <Switch>
-      <Route exact path="/" component={DKPlayer} />
-      <Route exact path="/:activeVideo" component={DKPlayer} />
-    </Switch>
-    <GlobalStyle />
+    <>
+      <Switch>
+        <Route exact path="/" component={DKPlayer} />
+        <Route exact path="/:activeVideo" component={DKPlayer} />
+      </Switch>
+      <GlobalStyle />
+    </>
   </BrowserRouter>
 );
 
