0ebaae9182e82aa2c80c137eec0dbd9576994d1b
Dario <dkemenovic@gmail.com>
2020-06-20 20:34:32 +0200

    Add label and input

--- src/components/Nightmode.js
+++ src/components/Nightmode.js
@@ -1,6 +1,13 @@
 import React from "react";
 import StyledNightmode from "./styles/StyledNightmode";
 
-const NightMode = (props) => <StyledNightmode>Nightmode!</StyledNightmode>;
+const NightMode = (props) => (
+  <StyledNightmode>
+    <span>NightMode:</span>
+    <label className="switch">
+      <input type="checkbox" checked={nightMode}></input>
+    </label>
+  </StyledNightmode>
+);
 
 export default NightMode;
