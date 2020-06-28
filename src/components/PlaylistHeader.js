b70fe4058ad9c51d45e519ab95a3a200cd0e5e60
Dario <dkemenovic@gmail.com>
2020-06-28 11:24:56 +0200

    Destructure props

--- src/components/PlaylistHeader.js
+++ src/components/PlaylistHeader.js
@@ -2,7 +2,7 @@ import React from "react";
 import StyledPlaylistHeader from "./styles/StyledPlaylistHeader";
 import StyledJourney from "./styles/StyledJourney";
 
-const PlaylistHeader = (props) => (
+const PlaylistHeader = ({ active, total }) => (
   <StyledPlaylistHeader>
     <p>{active.title}</p>
     <StyledJourney>
