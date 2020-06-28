2f47b3a22bd365aff478984f110c76def55a8742
Dario <dkemenovic@gmail.com>
2020-06-28 11:25:48 +0200

    Add props

--- src/components/PlaylistItem.js
+++ src/components/PlaylistItem.js
@@ -2,7 +2,9 @@ import React from "react";
 import StyledPlaylistitem from "./styles/StyledPlaylistItem";
 
 const PlaylistItem = (props) => (
-  <StyledPlaylistitem>PlaylistItem!!</StyledPlaylistitem>
+  <StyledPlaylistitem active={active} played={played}>
+    PlaylistItem!!
+  </StyledPlaylistitem>
 );
 
 export default PlaylistItem;
