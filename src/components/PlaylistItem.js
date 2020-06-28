4a77601e45c254fec9160916b3215ccda6ca81b5
Dario <dkemenovic@gmail.com>
2020-06-28 11:27:21 +0200

    Add video props

--- src/components/PlaylistItem.js
+++ src/components/PlaylistItem.js
@@ -1,7 +1,7 @@
 import React from "react";
 import StyledPlaylistitem from "./styles/StyledPlaylistItem";
 
-const PlaylistItem = ({ active, played }) => (
+const PlaylistItem = ({ video, active, played }) => (
   <StyledPlaylistitem active={active} played={played}>
     PlaylistItem!!
   </StyledPlaylistitem>
