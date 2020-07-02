409ef59ca06848b8e3953d24a5445d18f00b9ea7
Dario <dkemenovic@gmail.com>
2020-07-02 21:01:40 +0200

    Import StyledVideo

--- src/components/Video.js
+++ src/components/Video.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactPlayer from "react-player";
 import StyledVideWrapper from "./styles/StyledVideoWrapper";
+import StyledVide from "./styles/StyledVideo";
 
 const Video = (props) => <div>Video!</div>;
 
