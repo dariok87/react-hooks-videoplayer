3faba7db3134a4cb30a48541723d2b76f2d71b87
Dario <dkemenovic@gmail.com>
2020-07-02 21:02:11 +0200

    Fix typo

--- src/components/Video.js
+++ src/components/Video.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactPlayer from "react-player";
 import StyledVideWrapper from "./styles/StyledVideoWrapper";
-import StyledVide from "./styles/StyledVideo";
+import StyledVideo from "./styles/StyledVideo";
 
 const Video = (props) => <div>Video!</div>;
 
