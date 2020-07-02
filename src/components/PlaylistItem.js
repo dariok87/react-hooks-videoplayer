fac00e557531a5ad9002d02fa1cde0c24ddd835b
Dario <dkemenovic@gmail.com>
2020-07-02 20:57:52 +0200

    Add divs  with className

--- src/components/PlaylistItem.js
+++ src/components/PlaylistItem.js
@@ -4,6 +4,8 @@ import StyledPlaylistitem from "./styles/StyledPlaylistItem";
 const PlaylistItem = ({ video, active, played }) => (
   <StyledPlaylistitem active={active} played={played}>
     <div className="wbn-player__video-nr">{video.num}</div>
+    <div className="wbn-player__video-name">{video.title}</div>
+    <div className="wbn-player__video-time">{video.duration}</div>
   </StyledPlaylistitem>
 );
 
