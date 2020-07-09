46d180c48d3df211147ebe6fbf855b11edaa86a0
dariok87 <dkemenovic@gmail.com>
2020-07-09 23:16:49 +0200

    Create items with map method

--- src/components/containers/PlaylistItems.js
+++ src/components/containers/PlaylistItems.js
@@ -4,7 +4,13 @@ import StyledPlaylistitems from "../styles/StyledPlaylistitems";
 
 const PlaylistItems = ({ videos, active }) => (
   <StyledPlaylistitems>
-    <PlaylistItem />
+    {videos.map(video => (
+      <PlaylistItem 
+        key={video.id}
+        video={video}
+        active={video.id === active.id ? true : false}
+      />
+    ))}
   </StyledPlaylistitems>
 );
 
