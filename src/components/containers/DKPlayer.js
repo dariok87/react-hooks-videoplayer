aea622db410898972a2c7c0aa5e20d29624a1dee
dariok87 <dkemenovic@gmail.com>
2020-07-14 22:23:39 +0200

    Import state hooks

--- src/components/containers/DKPlayer.js
+++ src/components/containers/DKPlayer.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { ThemeProvider } from "styled-components";
 import Video from "../Video";
 import Playlist from "../containers/Playlist";
