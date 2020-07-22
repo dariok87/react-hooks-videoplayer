12ec6d9134785bbb08cb688db0d1b11974c517cf
dariok87 <dkemenovic@gmail.com>
2020-07-22 22:38:40 +0200

    Import high-order component

--- src/components/containers/PlaylistItems.js
+++ src/components/containers/PlaylistItems.js
@@ -1,5 +1,6 @@
 import React from "react";
 import PlaylistItem from "../PlaylistItem";
+import withLink from '../hoc/withLink';
 import StyledPlaylistitems from "../styles/StyledPlaylistitems";
 
 const PlaylistItems = ({ videos, active }) => (
